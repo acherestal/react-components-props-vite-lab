f451292edf8fc0ca24456609bc0a61d208b13b8a
Alanda <alandacherestal@gmail.com>
2025-12-03 14:21:09 -0500

    completed codes

---
+++ src/components/ArticleList.jsx
@@ -0,0 +1,22 @@
+//create a list of Article components
+
+import Article from "../components/Article";
+
+function ArticleList({ posts }) {
+  return (
+    <main>
+      {posts.map(post => (
+        <Article
+          key={post.id}
+          title={post.title}
+          date={post.date}
+          preview={post.preview}
+          minutes={post.minutes}
+        />
+      ))}
+    </main>
+  );
+}
+
+export default ArticleList;
+
